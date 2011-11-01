75f12b2fea355de7cb95b32d6e26887859b493ef
Vaporbook <aaron@readsocialapi.com>
2011-11-01 18:57:53 -0400

    fixed some issues and added zip file test

--- test.php
+++ test.php
@@ -1,13 +1,22 @@
 <?php
 
 require_once('./BookGluttonEpub.php');
+require_once('./BookGluttonZipEpub.php');
 
 $file = './H. G. Wells - The War of the Worlds.epub';
 
+echo "Opening $file as OPS in temp dir:\n";
+
 $epub = new BookGluttonEpub();
 $epub->open($file);
-
 print_r($epub->getMetaPairs());
+
+echo "Now opening $file as virtual zip (no filesystem on disk):\n";
+
+$epub = new BookGluttonZipEpub();
+$epub->loadZip($file);
+print_r($epub->getMetaPairs());
+
 
 
 ?>
